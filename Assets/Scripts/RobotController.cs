20a3491ede5fb6d3d43fa6c3c6e807dc96238b30
tjadams <tyler@virtualpowersystems.com>
2015-09-20 03:53:34 -0400

    Add movement and rotation of tanks

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotController : MonoBehaviour {
+
+	public Transform transform = null;
+
+	public float movementSpeed = 5.0f;
+	public float rotationSpeed = 1.0f;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			transform.Rotate((Vector3.down) * rotationSpeed);
+		} else if (Input.GetKey ("d")) {
+			transform.Rotate((Vector3.up) * rotationSpeed);
+		}
+
+		if (Input.GetKey ("w")) {
+			transform.position += transform.right * movementSpeed * Time.deltaTime;
+		} else if (Input.GetKey ("s")) {
+			transform.position -= transform.right * movementSpeed * Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Scripts/SecondRobotController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondRobotController : MonoBehaviour {
+	
+	public Transform transform = null;
+	
+	public float movementSpeed = 5.0f;
+	public float rotationSpeed = 1.0f;
+	
+	void Start () {
+		WebCamDevice[] devices = WebCamTexture.devices;
+		for( var i = 0 ; i < devices.Length; i++ )
+			Debug.Log(devices[i].name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Rotate((Vector3.down) * rotationSpeed);
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Rotate((Vector3.up) * rotationSpeed);
+		}
+		
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += transform.right * movementSpeed * Time.deltaTime;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position -= transform.right * movementSpeed * Time.deltaTime;
+		}
+	}
+}
